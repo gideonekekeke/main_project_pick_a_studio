ae1fdf40dbe098f56440f4fcdc774ec697916c29
AnyamahNdidi <theo4felix@gmail.com>
2021-09-09 02:57:57 +0100

    the first commit for the project

---
+++ src/components/oluruda/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ModalButton from "./Body/Body";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import { HeroBg } from "./HeroSection/HeroElement";
+
+
+function Home() {
+
+  return (
+    <div>
+
+      <HeroSection />
+      <ModalButton />
+
+    </div>
+  );
+}
+
+export default Home;
